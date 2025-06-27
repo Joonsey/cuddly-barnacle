5058e9ef25b143809b753f239e1c9e6e0d14ad04
Johannes <johannes.ronning@outlook.com>
2025-06-27 19:03:02 +0200

    cleaning up tracks

--- src/tracks.zig
+++ src/tracks.zig
@@ -4,6 +4,8 @@ const rl = @import("raylib");
 const entity = @import("entity.zig");
 const renderer = @import("renderer.zig");
 
+const track_halflife = 12 * std.time.ms_per_s;
+
 pub const Tracks = struct {
     const Query = struct {
         entity: entity.EntityId,
@@ -13,9 +15,15 @@ pub const Tracks = struct {
     const Track = struct {
         position: rl.Vector2,
         rotation: f32 = 0,
+        start_time: i64,
     };
 
-    tracks: std.AutoHashMapUnmanaged(Query, std.ArrayListUnmanaged(Track)) = .{},
+    const _Tracks = struct {
+        arr: std.ArrayListUnmanaged(Track) = .{},
+        start_time: i64,
+    };
+
+    tracks: std.AutoHashMapUnmanaged(Query, _Tracks) = .{},
     indexes: std.AutoHashMapUnmanaged(entity.EntityId, usize) = .{},
 
     allocator: std.mem.Allocator,
@@ -29,7 +37,7 @@ pub const Tracks = struct {
 
     pub fn deinit(self: *Self) void {
         var iter = self.tracks.valueIterator();
-        while (iter.next()) |track| track.clearAndFree(self.allocator);
+        while (iter.next()) |track| track.arr.clearAndFree(self.allocator);
         self.tracks.clearAndFree(self.allocator);
         self.indexes.clearAndFree(self.allocator);
     }
@@ -45,8 +53,8 @@ pub const Tracks = struct {
                                 break :blk 0;
                             };
                             const query: Query = .{ .index = index, .entity = @intCast(i) };
-                            var tracks: std.ArrayListUnmanaged(Track) = self.tracks.get(query) orelse .{};
-                            tracks.append(self.allocator, .{ .position = transform.position, .rotation = transform.rotation }) catch unreachable;
+                            var tracks: _Tracks = self.tracks.get(query) orelse .{ .start_time = std.time.milliTimestamp() };
+                            tracks.arr.append(self.allocator, .{ .position = transform.position, .rotation = transform.rotation, .start_time = std.time.milliTimestamp() }) catch unreachable;
                             self.tracks.put(self.allocator, query, tracks) catch unreachable;
                         } else {
                             if (self.indexes.getPtr(@intCast(i))) |index| {
@@ -58,6 +66,13 @@ pub const Tracks = struct {
                 }
             }
         }
+
+        var iter = self.tracks.valueIterator();
+        while (iter.next()) |tracks| {
+            if (tracks.start_time + (track_halflife * 2) < std.time.milliTimestamp()) {
+                tracks.arr.clearAndFree(self.allocator);
+            }
+        }
     }
 
     pub fn draw(self: Self, camera: renderer.Camera) void {
@@ -67,15 +82,21 @@ pub const Tracks = struct {
 
         const car_radius = 5;
         while (iter.next()) |tracks| {
-            for (tracks.items) |track| {
+            if (tracks.arr.items.len == 0) continue;
+            for (tracks.arr.items) |track| {
                 const forward = rl.Vector2{ .x = @cos(track.rotation), .y = @sin(track.rotation) };
                 const perp = rl.Vector2{ .x = -forward.y, .y = forward.x };
 
                 left.append(self.allocator, camera.get_relative_position(track.position.add(perp.scale(car_radius)))) catch unreachable;
                 right.append(self.allocator, camera.get_relative_position(track.position.subtract(perp.scale(car_radius)))) catch unreachable;
             }
-            rl.drawLineStrip(left.items, .black);
-            rl.drawLineStrip(right.items, .black);
+            const now = std.time.milliTimestamp();
+            var color: rl.Color = .black;
+            const t_time_halflife = tracks.start_time + (track_halflife * 2);
+            const delta_time: f32 = @floatFromInt(t_time_halflife - now);
+            color = color.alpha(delta_time / @as(f32, @floatFromInt(track_halflife)));
+            rl.drawLineStrip(left.items, color);
+            rl.drawLineStrip(right.items, color);
             left.clearAndFree(self.allocator);
             right.clearAndFree(self.allocator);
         }
